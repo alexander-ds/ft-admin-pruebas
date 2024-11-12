1aec3ec1de4a718f82fc334cbc169d99fd3328b6
Deiby Deaquiz <alxds.ad@gmail.com>
2024-11-12 07:33:39 -0500

    feat: se agrega la bse del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { IngresarComponent } from './ingresar/ingresar.component';
+import { ConsultarComponent } from './consultar/consultar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,IngresarComponent,ConsultarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
